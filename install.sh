8558091916b7683ceca38ba038d37221e0d38343
nethan <nethan.win@protonmail.com>
2024-09-14 23:57:49 +0300

    add starship conf

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ sudo pacman -S fish
 set CONF ~/.config
 sudo mkdir -p $CONF/calc $CONF/bash $CONF/rustup $CONF/cargo $CONF/gtkrc-2.0 $CONF/git $CONF/librewolf/private $CONF/librewolf/public
 sudo mkdir -p $CONF/android $CONF/fonts $CONF/git $CONF/gnupg $CONF/java $CONF/mono $CONF/npm $CONF/pki $CONF/ssh $CONF/swt $CONF/vim $CONF/yay
+
+cp ./starship.toml ~/.config/starship.toml
